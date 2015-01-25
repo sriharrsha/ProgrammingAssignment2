e145f69f13a024d2b1d0c8eaa123d6f9bdaaf6c0
Sri Harrsha <sriharrsha@live.com>
2015-01-26 00:26:31 +0530

    Completely Solved
    Solved

--- cachematrix.R
+++ cachematrix.R
@@ -4,30 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-      x <<- y
-      m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Cache Solving Function 
+
 
 cacheSolve <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setinverse(m)
+  m
+  
 }
